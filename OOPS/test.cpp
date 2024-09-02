73c776d662fe241394f52c3cd3cd6fe33dacf1d4
Mugunthan <mugunthan@Mugunthans-MacBook-Air.local>
2024-09-02 10:02:07 +0530

    milestone 2 completed

--- OOPS/test.cpp
+++ OOPS/test.cpp
@@ -21,22 +21,32 @@ public:
 
     void travel() { 
         cout << "TimeMachine traveling through time with power level " << this->powerLevel << "!" << endl; 
+        numberOfTravels++; 
+    }
+    
+    static int getNumberOfTravels(){
+        return numberOfTravels;
     }
 
 private:
     int powerLevel; 
+    static int numberOfTravels;
 };
+
+int TimeMachine::numberOfTravels = 0; 
 
 class Traveler {
 public:
     Traveler() {
         name = "";        
         destination = "";  
+        numberOfTravelers++; 
     }
 
     Traveler(string travelerName, string dest) {
         this->name = travelerName;        
         this->destination = dest;          
+        numberOfTravelers++; 
     }
 
     void setName(string travelerName) { 
@@ -59,23 +69,40 @@ public:
         cout << "Traveler: " << this->name << ", Destination: " << this->destination << endl; 
     }
 
+    static int getNumberOfTravelers(){
+        return numberOfTravelers;
+    }
+
 private:
     string name; 
     string destination; 
+    static int numberOfTravelers;
 };
 
+int Traveler::numberOfTravelers = 0; 
 int main() {
-   Traveler travelers[3] = {
+    Traveler travelers[3] = {
         Traveler("John Doe", "Future - 2050"),
         Traveler("Alice Smith", "Past - 1800"),
         Traveler("Bob Johnson", "Present - 2024")
     };
+
     TimeMachine tm;
     tm.setPowerLevel(100);
     cout << "Power Level: " << tm.getPowerLevel() << endl;    
-    tm.travel(); 
+    tm.travel();
+
     for (int i = 0; i < 3; ++i) {
         travelers[i].displayTravelerInfo();
     }
+
+    Traveler* traveler1 = new Traveler("Mugunthan", "Past-2015");
+    traveler1->displayTravelerInfo();
+    delete traveler1; 
+    int totalTravels = TimeMachine::getNumberOfTravels(); 
+    int totalTravelers = Traveler::getNumberOfTravelers(); 
+    cout << "Total Travels: " << totalTravels << endl;
+    cout << "Total Travelers: " << totalTravelers << endl;
+
     return 0;
 }
