bd9db98c3235f8f63241acaeb97dd78513ea245c
Mugunthan <mugunthan@Mugunthans-MacBook-Air.local>
2024-10-23 11:08:30 +0530

    milestone-3 completed

--- OOPS/test.cpp
+++ OOPS/test.cpp
@@ -1,85 +1,107 @@
 #include <iostream>
 using namespace std;
 
-class TimeMachine {
+class Machine {
+public:
+    virtual void travel() = 0;
+
+    virtual ~Machine() {
+        cout << "Machine destructor called." << endl;
+    }
+};
+
+class TimeMachine : public Machine {
 public:
     TimeMachine() {
-        powerLevel = 0; 
+        powerLevel = 0;
+        cout << "TimeMachine default constructor called." << endl;
     }
 
     TimeMachine(int level) {
-        this->powerLevel = level; 
+        powerLevel = level;
+        cout << "TimeMachine parameterized constructor called." << endl;
     }
 
-    void setPowerLevel(int level) { 
-        this->powerLevel = level; 
+    ~TimeMachine() {
+        cout << "TimeMachine destructor called." << endl;
     }
 
-    int getPowerLevel() { 
-        return this->powerLevel; 
+    void setPowerLevel(int level) {
+        powerLevel = level;
     }
 
-    void travel() { 
-        cout << "TimeMachine traveling through time with power level " << this->powerLevel << "!" << endl; 
-        numberOfTravels++; 
+    int getPowerLevel() {
+        return powerLevel;
     }
-    
-    static int getNumberOfTravels(){
+
+    void travel() override {
+        cout << "TimeMachine traveling with power level " << powerLevel << "!" << endl;
+        numberOfTravels++;
+    }
+
+    static int getNumberOfTravels() {
         return numberOfTravels;
     }
 
 private:
-    int powerLevel; 
+    int powerLevel;
     static int numberOfTravels;
 };
 
-int TimeMachine::numberOfTravels = 0; 
+int TimeMachine::numberOfTravels = 0;
 
 class Traveler {
 public:
     Traveler() {
-        name = "";        
-        destination = "";  
-        numberOfTravelers++; 
+        name = "Unknown";
+        destination = "Unknown";
+        cout << "Traveler default constructor called." << endl;
+        numberOfTravelers++;
     }
 
     Traveler(string travelerName, string dest) {
-        this->name = travelerName;        
-        this->destination = dest;          
-        numberOfTravelers++; 
+        name = travelerName;
+        destination = dest;
+        cout << "Traveler parameterized constructor called." << endl;
+        numberOfTravelers++;
     }
 
-    void setName(string travelerName) { 
-        this->name = travelerName; 
+    ~Traveler() {
+        cout << "Traveler destructor called for " << name << "." << endl;
     }
 
-    void setDestination(string dest) { 
-        this->destination = dest; 
+    void setName(string travelerName) {
+        name = travelerName;
     }
 
-    string getName() { 
-        return this->name; 
+    void setDestination(string dest) {
+        destination = dest;
     }
 
-    string getDestination() { 
-        return this->destination; 
+    string getName() {
+        return name;
+    }
+
+    string getDestination() {
+        return destination;
     }
 
     void displayTravelerInfo() {
-        cout << "Traveler: " << this->name << ", Destination: " << this->destination << endl; 
+        cout << "Traveler: " << name << ", Destination: " << destination << endl;
     }
 
-    static int getNumberOfTravelers(){
+    static int getNumberOfTravelers() {
         return numberOfTravelers;
     }
 
 private:
-    string name; 
-    string destination; 
+    string name;
+    string destination;
     static int numberOfTravelers;
 };
 
-int Traveler::numberOfTravelers = 0; 
+int Traveler::numberOfTravelers = 0;
+
 int main() {
     Traveler travelers[3] = {
         Traveler("John Doe", "Future - 2050"),
@@ -89,18 +111,19 @@ int main() {
 
     TimeMachine tm;
     tm.setPowerLevel(100);
-    cout << "Power Level: " << tm.getPowerLevel() << endl;    
+    cout << "Power Level: " << tm.getPowerLevel() << endl;
     tm.travel();
 
     for (int i = 0; i < 3; ++i) {
         travelers[i].displayTravelerInfo();
     }
 
-    Traveler* traveler1 = new Traveler("Mugunthan", "Past-2015");
+    Traveler* traveler1 = new Traveler("Mugunthan", "Past - 2015");
     traveler1->displayTravelerInfo();
-    delete traveler1; 
-    int totalTravels = TimeMachine::getNumberOfTravels(); 
-    int totalTravelers = Traveler::getNumberOfTravelers(); 
+    delete traveler1;
+
+    int totalTravels = TimeMachine::getNumberOfTravels();
+    int totalTravelers = Traveler::getNumberOfTravelers();
     cout << "Total Travels: " << totalTravels << endl;
     cout << "Total Travelers: " << totalTravelers << endl;
 
